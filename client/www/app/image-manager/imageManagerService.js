8c1105e3a1a8ffd8e139819648f2624eaaa00258
Ryan Atkinson <rjatkinson2@gmail.com>
2015-04-23 12:36:33 -0700

    Refactor functioning mobile uploads

--- client/www/app/image-manager/imageManagerService.js
+++ client/www/app/image-manager/imageManagerService.js
@@ -15,41 +15,20 @@
       return instance;
 
       // implementation of functions
-      function postSelectedImage(image) {
-        var uploadUrl = "http://localhost:4568/api/images/user/s3upload";
-        var fd = new FormData();
-        fd.append('file1', image);
-        fd.append('userName', 'Joey');
-        alert('down here now');
-        $http.post(uploadUrl, fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        })
-        .success(function(test){
-          alert(test);
-          alert('success');
-        })
-        .error(function(test2){
-          alert(test2);
-          alert('error');
-        });
-
-        // $upload.upload({
-        //   url: 'http://localhost:4568/api/images/user/s3upload',
-        //   fields: {
-        //       'userName': 'ryan'
-        //   },
-        //   file: image
-        // })
-        // .progress(function (evt) {
-        //   var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-        //   alert('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-        // })
-        // .success(function (data, status, headers, config) {
-        //   alert('file ' + config.file.name + 'uploaded. Response: ' + JSON.stringify(data));
-        // });
+      function postSelectedImage(files) {
+        if (files && files.length) {
+          var file = files[0];
+          var fields = { 'userName': 'madeline' };
+          var url = 'http://localhost:4568/api/images/user/s3upload';
+          $upload.upload({ url: url, fields: fields, file: file })
+          .progress(function (evt) {
+              var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+              console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+          })
+          .success(function (data, status, headers, config) {
+              alert('file ' + config.file.name + 'uploaded. Response: ' + JSON.stringify(data));
+          });
+        }
       }
-
     }
-
 })();
--- client/www/app/login/LoginController.js
+++ client/www/app/login/LoginController.js
@@ -4,9 +4,9 @@
     .module('wildDonut')
     .controller('LoginController', LoginController);
 
-  LoginController.$inject = ['$scope', '$location', '$state', '$upload', 'ImageManager', 'Authenticator', 'Facebook'];
+  LoginController.$inject = ['$scope', '$location', '$state', 'ImageManager', 'Authenticator', 'Facebook'];
 
-  function LoginController($scope, $location, $state, $upload, ImageManager, Authenticator){
+  function LoginController($scope, $location, $state, ImageManager, Authenticator){
     // initialize empty user
     $scope.user = {};
 
@@ -28,55 +28,9 @@
       });
     };
 
-    $scope.$watch('files', function () {
-      $scope.upload($scope.files);
-    });
-
     $scope.upload = function (files) {
-        if (files && files.length) {
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                $upload.upload({
-                    url: 'http://localhost:4568/api/images/user/s3upload',
-                    fields: {
-                        'userName': $scope.username
-                    },
-                    file: file
-                }).progress(function (evt) {
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    console.log('progress: ' + progressPercentage + '% ' +
-                                evt.config.file.name);
-                }).success(function (data, status, headers, config) {
-                    console.log('file ' + config.file.name + 'uploaded. Response: ' +
-                                JSON.stringify(data));
-                });
-            }
-        }
+      ImageManager.postSelectedImage(files);
     };
-
-    // $scope.$watch('files', function () {
-    //   console.log('watch has fired');
-    //   console.log($scope.files);
-    //   $scope.upload($scope.files);
-    // });
-
-    // $scope.upload = function(files){
-    //   console.log('upload has fired');
-    //   console.log(files);
-    // };
-
-    // $scope.pickPic = function(){
-    //   window.imagePicker.getPictures(
-    //     function(image) {
-    //       alert('results are the following: ' + image);
-    //       ImageManager.postSelectedImage(image);
-    //     }, function (error) {
-    //       console.log('Error: ' + error);
-    //     }, {
-    //       maximumImagesCount: 1
-    //     }
-    //   );
-    // };
   }
 
 })();
