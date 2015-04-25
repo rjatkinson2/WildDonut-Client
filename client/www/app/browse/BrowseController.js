21f8e1b7a6d1deed9e1c9058e67f231dfa850384
Ryan Atkinson <rjatkinson2@gmail.com>
2015-04-24 20:59:29 -0700

    (feat) Add image upload functionality to class settings page

--- client/www/app/browse/BrowseController.js
+++ client/www/app/browse/BrowseController.js
@@ -4,37 +4,37 @@
     .module('wildDonut')
     .controller('BrowseController', BrowseController);
 
-    BrowseController.$inject = ['$scope', '$location', 'CollectionManager', 'ReviewManager', 'State'];
+  BrowseController.$inject = ['$scope', '$location', 'CollectionManager', 'ReviewManager', 'State'];
 
-    function BrowseController($scope, $location, CollectionManager, ReviewManager, State) {
-      $scope.stars = {};
+  function BrowseController($scope, $location, CollectionManager, ReviewManager, State) {
+    $scope.stars = {};
 
-      $scope.getAvailableClasses = function() {
-        CollectionManager.getAvailableClasses().then(function(classes) {
-          $scope.classes = classes;
-          $scope.classes.forEach(function(classInstance){
-            classInstance.stars = ReviewManager.getStars(classInstance.avg_rating);
-          });
+    $scope.getAvailableClasses = function() {
+      CollectionManager.getAvailableClasses().then(function(classes) {
+        $scope.classes = classes;
+        $scope.classes.forEach(function(classInstance){
+          classInstance.stars = ReviewManager.getStars(classInstance.avg_rating);
         });
-      };
+      });
+    };
 
-      $scope.viewProfile = function(classInstance) {
-        $location.path('/' + classInstance.teacher.username + '/profile');
-      };
+    $scope.viewProfile = function(classInstance) {
+      $location.path('/' + classInstance.teacher.username + '/profile');
+    };
 
-      $scope.viewClass = function(classInstance) {
-        $location.path('/' + classInstance.teacher.username + '/teacher/classes/' + classInstance._id);
-      };
+    $scope.viewClass = function(classInstance) {
+      $location.path('/' + classInstance.teacher.username + '/teacher/classes/' + classInstance._id);
+    };
 
-      $scope.getStarsLength = function(num){
-        return new Array(num);
-      };
+    $scope.getStarsLength = function(num){
+      return new Array(num);
+    };
 
-      $scope.init = function() {
-        $scope.getAvailableClasses();
-      };
+    $scope.init = function() {
+      $scope.getAvailableClasses();
+    };
 
-      $scope.init();
-    }
+    $scope.init();
+  }
 
 })();
--- client/www/app/teacher/teacher-edit-class/TeacherEditClassController.js
+++ client/www/app/teacher/teacher-edit-class/TeacherEditClassController.js
@@ -4,9 +4,9 @@
     .module('wildDonut')
     .controller('TeacherEditClassController', TeacherEditClassController);
 
-  TeacherEditClassController.$inject = ['$scope', '$stateParams', '$state', 'ClassManager', 'State'];
+  TeacherEditClassController.$inject = ['$scope', '$stateParams', '$state', 'ImageManager', 'ClassManager', 'State'];
 
-  function TeacherEditClassController($scope, $stateParams, $state, ClassManager, State){
+  function TeacherEditClassController($scope, $stateParams, $state, ImageManager, ClassManager, State){
 
     $scope.class_id = $stateParams.id;
 
@@ -26,6 +26,12 @@
     $scope.updateClass = function(){
       ClassManager.editClass($scope.classInformation, $scope.class_id).then(function(response){
         $state.go('manageClasses', {username: State.user.username}, {reload: true});
+      });
+    };
+
+    $scope.upload = function (files) {
+      ImageManager.postSelectedImage(files, State.username, 'user', function(url){
+        $scope.classInformation.picture_url = url;
       });
     };
 
